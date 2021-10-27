c3c8d34f402f8cf73d586f1c268becc34b30e36c
Philippe Douelle <douelle.p@gmail.com>
2021-10-27 12:12:20 +0200

    RelationshipNotFound

---
+++ pdouelle.Errors/RelationshipNotFound.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using Newtonsoft.Json;
+
+namespace pdouelle.Errors
+{
+    public class RelationshipNotFound : ErrorObject
+    {
+        public RelationshipNotFound(MemberInfo type, object value)
+        {
+            Title = $"The requested relationship does not exist.";
+            Detail = $"Resource of type '{type}' does not contain a relationship with these properties '{JsonConvert.SerializeObject(value)}'.";
+        }
+    }
+}
